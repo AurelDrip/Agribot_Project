cf3d3e22cb6196ebd4641d78b9fd75daf5cf2642
aurelien.lohmann <aurelien.lohmann@ecam.fr>
2023-08-20 19:47:14 +0100

    Last commit

--- src/processing_data.py
+++ src/processing_data.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python3
 import rospy
+from nav_msgs.msg import Odometry
 from sensor_msgs.msg import NavSatFix
-from geometry_msgs.msg import PointStamped
 import geonav_conversions as gc
 
 class ProcessingDataNode:
     def __init__(self):
         rospy.init_node('processing_data')
+
+        #Subscribe to the gps data
         self.gps_sub = rospy.Subscriber('/gps/duro/fix', NavSatFix, self.gps_callback)
-        self.true_pose_pub = rospy.Publisher('/true_pose', PointStamped, queue_size=1)
+
+        #Publish data as point stamped
+        self.true_pose_pub = rospy.Publisher('/true_pose', Odometry, queue_size=1)
+        self.gpsdata = Odometry()  # GPS data
+        self.gpsdata.header.frame_id = 'odom'
+        # Flag to initialise first GPS data
+        self.gps = True 
+
+
 
     def gps_callback(self, msg):
-        # Extract latitude and longitude from the NavSatFix message
-        latitude = msg.latitude
-        longitude = msg.longitude
-
-        # Replace the origin_lat and origin_lon with your desired origin coordinates
-        origin_lat = 0.0
-        origin_lon = 0.0
+        if self.gps is True:
+            print("data received")
+            self.lat_origin = msg.latitude
+            self.longi_origin = msg.longitude
+            self.gps = False
+        # Extract latitude and longitude from the NavSatFix message            
+        lat = msg.latitude
+        long = msg.longitude
 
         # Convert latitude and longitude to UTM coordinates
-        x, y = gc.ll2xy(latitude, longitude, origin_lat, origin_lon)
+        self.xgps, self.ygps = gc.ll2xy(
+            lat, long, self.lat_origin, self.longi_origin)
 
         # Publish the converted UTM coordinates on the /true_pose topic
-        true_pose_msg = PointStamped()
-        true_pose_msg.header = msg.header
-        true_pose_msg.point.x = x
-        true_pose_msg.point.y = y
-        true_pose_msg.point.z = 0.0  # Assuming z-coordinate is 0 for simplicity
+        self.gpsdata.pose.pose.position.x = self.xgps
+        self.gpsdata.pose.pose.position.y = self.ygps
 
-        self.true_pose_pub.publish(true_pose_msg)
+        self.true_pose_pub.publish(self.gpsdata)
 
 if __name__ == "__main__":
     try:
